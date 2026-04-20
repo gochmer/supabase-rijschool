6fc16a850ab32376ddb57d14e8e327746064c8c0
Gochmer <gochmershahab@gmail.com>
2026-04-21 01:42:27 +0200

    fix shadcn button slot import

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,4 +1,4 @@
-beterzo import * as React from "react"
+import * as React from "react"
 import { cva, type VariantProps } from "class-variance-authority"
 import { Slot } from "@radix-ui/react-slot"
 
