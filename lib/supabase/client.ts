fc9037b69a9f325f78771db393390c94ac82b9e7
Gochmer <gochmershahab@gmail.com>
2026-04-20 22:21:02 +0200

    first commit

---
+++ lib/supabase/client.ts
@@ -0,0 +1,25 @@
+import { createClient as createSupabaseClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+function getSupabaseEnv() {
+  if (!supabaseUrl) {
+    throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_URL");
+  }
+
+  if (!supabaseAnonKey) {
+    throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_ANON_KEY");
+  }
+
+  return {
+    supabaseUrl,
+    supabaseAnonKey,
+  };
+}
+
+export function createClient() {
+  const { supabaseUrl, supabaseAnonKey } = getSupabaseEnv();
+
+  return createSupabaseClient(supabaseUrl, supabaseAnonKey);
+}
